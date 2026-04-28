906357f4caa1943ba75fbd4c3ba2358d619888f2
anotherdevv <196869443+anotherdevv@users.noreply.github.com>
2026-04-27 19:19:25 -0700

    printing twice to the console.
    one with printing text on the trimStart() method "After using trimStart()....".
    
    and another with printing trimStart() method being used for the "userInput" variable.
    
    thus, cleaning out the whitespace at the beginning (trimStart()).

--- string-formatter.js
+++ string-formatter.js
@@ -7,3 +7,5 @@ console.log("Result of trimming whitespace from both ends:");
 console.log(cleanedInput);
 
 const trimmedStart = userInput.trimStart();
+console.log("After using the trimStart() method, leading spaces removed:");
+console.log(trimmedStart);
