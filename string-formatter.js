dfa58c6b9ec68b5d4e0a9e9c8572d94822ba2df7
anotherdevv <196869443+anotherdevv@users.noreply.github.com>
2026-04-27 19:02:56 -0700

    printed the text of the "result of trimming whitespace from both ends."
    then, printed again the "cleanedInput" varible - showing how the trim() method cleared out the whitespace from both sides of the initial "userInput" varible = Hello, World!.

--- string-formatter.js
+++ string-formatter.js
@@ -3,3 +3,5 @@ console.log("Original input:");
 console.log(userInput);
 
 const cleanedInput = userInput.trim();
+console.log("Result of trimming whitespace from both ends:");
+console.log(cleanedInput);
