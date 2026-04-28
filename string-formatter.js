272cfdffe6d4dda91166aa389bdd61fa937de957
anotherdevv <196869443+anotherdevv@users.noreply.github.com>
2026-04-27 19:21:46 -0700

    create a variable called trimmedEnd and assign it the result of using the trimEnd() method on the userInput variable.

--- string-formatter.js
+++ string-formatter.js
@@ -9,3 +9,5 @@ console.log(cleanedInput);
 const trimmedStart = userInput.trimStart();
 console.log("After using the trimStart() method, leading spaces removed:");
 console.log(trimmedStart);
+
+const trimmedEnd = userInput.trimEnd();
